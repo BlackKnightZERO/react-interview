fd9fa835ab4d8c71a15fe1d4e805e05bff5f4131
Arif Faysal <ayon.arif.10@gmail.com>
2021-11-12 23:33:52 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+// import './App.css';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
 
   const [counter, setCounter] = useState(0);
-  // const [jsonData, setjsonData] = userState([]);
+  const [jsonData, setjsonData] = useState('');
+  const [userData, setUserData] = useState([]);
 
+  const getSingleUserName = (singleUser) => {
+    const { name : {first, last}} = singleUser;
+    return `${first} ${last}`;
+  }
+
+  const getSingleUserImage = (singleUser) => {
+    const { picture: {thumbnail}} = singleUser;
+    return thumbnail;
+  }
+  useEffect(() => {
+    fetchJsonData().then((userData) => {
+      const stringifyData = JSON.stringify(userData, null, 2);
+      setjsonData(stringifyData || 'No User Found');
+      
+      console.log(userData);
+      setUserData(userData.data.results);
+    });
+  },[]);
 
   async function fetchJsonData() {
     try {
       const response = await axios.get('https://randomuser.me/api/');
-      console.log(response);
+      return response;
+
+      // let stringifyData = JSON.stringify(response.data.results, null, 2);
+      // let stringifyData = JSON.stringify(response.data.results);
+      // setjsonData(stringifyData);
+      // console.log(response.data.results);
+      // setUserData(response.data.results);
+
     } catch (error) {
       console.error(error);
     }
@@ -42,8 +68,16 @@ function App() {
       <button onClick={ () => setCounter( counter + 1 ) }>increment</button>
       <p>Problem 2</p>
       <button onClick={fetchJsonData}>fetch user data</button>
-      {/* <pre>{jsonData}</pre> */}
+      <pre>{jsonData}</pre>
       <p>Problem 3</p>
+      {
+        userData.map((singleUser, index) => (
+          <div key={index}>
+            <img src={getSingleUserImage(singleUser)} />
+            <p><i>{getSingleUserName(singleUser)}</i></p>
+          </div>
+        ))
+      }
       <p>Problem 4</p>
     </div>
   );
