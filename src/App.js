21a0f59058924cc796877c53cc62534d25fe6712
Arif Faysal <ayon.arif.10@gmail.com>
2021-11-12 22:32:09 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+  // const [jsonData, setjsonData] = userState([]);
+
+
+  async function fetchJsonData() {
+    try {
+      const response = await axios.get('https://randomuser.me/api/');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +33,18 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <h1>REACT INTERVIEW</h1>
+      <hr></hr>
+      <p>Problem 1</p>
+      <p>{counter}</p>
+      <button onClick={ () => setCounter( counter + 1 ) }>increment</button>
+      <p>Problem 2</p>
+      <button onClick={fetchJsonData}>fetch user data</button>
+      {/* <pre>{jsonData}</pre> */}
+      <p>Problem 3</p>
+      <p>Problem 4</p>
     </div>
   );
 }
