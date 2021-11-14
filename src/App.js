1b4fe1a3d0e1715abb3b6684aea1e7e4d3cae4d3
Arif Faysal <ayon.arif.10@gmail.com>
2021-11-13 19:37:43 -0500

    final

--- src/App.js
+++ src/App.js
@@ -3,28 +3,69 @@ import logo from './logo.svg';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+interface SingleUserName {
+  first: String;
+  last: String;
+}
+
+interface SingleUserImage {
+  thumbnail: String;
+}
+
+interface SingleUser {
+  name: SingleUserName;
+  picture: SingleUserImage;
+  // picture: any;
+}
+
 function App() {
 
   const [counter, setCounter] = useState(0);
   const [jsonData, setjsonData] = useState('');
   const [userData, setUserData] = useState([]);
+  const [nextPageNumber, setnextPageNumber] = useState(1);
 
-  const getSingleUserName = (singleUser) => {
+  const getSingleUserName = (singleUser: SingleUser) => {
     const { name : {first, last}} = singleUser;
     return `${first} ${last}`;
   }
 
-  const getSingleUserImage = (singleUser) => {
+  const getSingleUserImage = (singleUser: SingleUser) => {
     const { picture: {thumbnail}} = singleUser;
     return thumbnail;
   }
+
+  // const fetchAndAppendNewUser = (pageNumber : number = 1) => {
+    const fetchAndAppendNewUser = (pageNumber : number) => {
+
+      let url = `https://randomuser.me/api?page=${pageNumber}`;
+      
+      return axios.get(url)
+                  .then(({ data }) => {
+
+                    if(data===undefined) return;
+
+                    console.log(data.results);
+                    const newUser = [
+                      ...userData,
+                      ...data.results,
+                    ]
+                    setUserData(newUser);
+                    setjsonData(JSON.stringify(data, null, 2));
+                  })
+                  .catch((error) => {
+                    console.error(error);
+                  })
+  }
+
   useEffect(() => {
-    fetchJsonData().then((userData) => {
-      const stringifyData = JSON.stringify(userData, null, 2);
+    fetchJsonData(nextPageNumber).then((userData) => {
+      const stringifyData = JSON.stringify(userData.data, null, 2);
       setjsonData(stringifyData || 'No User Found');
       
       console.log(userData);
       setUserData(userData.data.results);
+      setnextPageNumber(userData.data.info.page + 1);
     });
   },[]);
 
@@ -63,22 +104,28 @@ function App() {
 
       <h1>REACT INTERVIEW</h1>
       <hr></hr>
-      <p>Problem 1</p>
+
+      <p style={{color:'red'}}>Problem 1</p>
       <p>{counter}</p>
       <button onClick={ () => setCounter( counter + 1 ) }>increment</button>
-      <p>Problem 2</p>
-      <button onClick={fetchJsonData}>fetch user data</button>
+      
+      <p style={{color:'red'}}>Problem 2</p>
+      {/* <button onClick={fetchJsonData}>fetch user data</button> */}
       <pre>{jsonData}</pre>
-      <p>Problem 3</p>
+      
+      <p style={{color:'red'}}>Problem 3</p>
+      <div style={{display:'flex', flexWrap:'wrap'}}>
       {
-        userData.map((singleUser, index) => (
-          <div key={index}>
+        userData.map((singleUser:SingleUserName, index) => (
+          <div key={index} style={{ margin:'5px' }}>
             <img src={getSingleUserImage(singleUser)} />
             <p><i>{getSingleUserName(singleUser)}</i></p>
           </div>
         ))
       }
-      <p>Problem 4</p>
+      </div>
+      <p style={{color:'red'}}>Problem 4</p>
+      <button onClick={fetchAndAppendNewUser}>fetch user data</button>
     </div>
   );
 }
